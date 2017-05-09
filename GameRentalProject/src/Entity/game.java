7c20ace93ff8432812d7b3d7a5835f891868faca
Pypylna <paupawlik@gmail.com>
2017-05-09 21:14:47 +0200

    first commit

---
+++ GameRentalProject/src/Entity/game.java
@@ -0,0 +1,18 @@
+package Entity;
+
+public class game {
+//#todo uzupelnic klase o pozostale pola i metody
+	//#todo gettery i settery do name i worth
+	
+	private int id;
+	private String name;
+	private float worth;		//wartosc
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+}
